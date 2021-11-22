11d1b661ce6120634d9ed753e1755a00c30b0786
IanMayther <maytheri@gmail.com>
2021-11-22 06:02:21 -0800

    Re-labeled window, adding views selections

--- calendar_app.py
+++ calendar_app.py
@@ -1,17 +1,23 @@
 import PySimpleGUI as sg
 
+sg.theme('LightGrey1')
+
+view_frame = [
+    [sg.Text("View", justification= 'r', key= '--View--'),sg.Combo(['Month','Week'])]
+]
+
 layout = [
-    [sg.Text("Hello Moose!")],
+    [sg.Frame('Views', view_frame)],
     [sg.Button("OK")]
 ]
 
 #create window
-window = sg.Window("Demo", layout, margins=(200, 300))
+window = sg.Window("Install Schedule", layout, margins=(300, 200))
 
 #create the event loop
 while True:
     event, values = window.read()
-    if event == "OK" or event == sg.WIN_CLOSED():
+    if event == "OK" or event == None:
         break
 
 window.close()
